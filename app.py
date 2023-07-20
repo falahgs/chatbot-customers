0a1468ed7a19f996e8a739635e403f378dceb4da
Falah Gate Salieh <digital_fgs@yahoo.com>
2023-07-20 14:57:40 +0300

    upload

--- app.py
+++ app.py
@@ -1,45 +1,115 @@
-# Imports
-import os 
-from langchain.llms import OpenAI,HuggingFaceHub,GPT4All
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
 import streamlit as st
-from streamlit_chat import message
-import pandas as pd
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.chat_models import ChatOpenAI
-import matplotlib.pyplot as plt
-import seaborn as sns
-api_token="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" 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_token
-df = pd.read_csv('mall_customers_data.csv')
-llm= HuggingFaceHub(
-        repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
-        model_kwargs={
-            "max_new_tokens":1000,
-            "top_k": 10,
-            "temperature": 1,
-            "top_p": 0.5,
-        }
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+#from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+import os
+from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+# Sidebar contents
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
     )
-#llm = ChatOpenAI(model_name = model_id, temperature=0)
-llm=llm#HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"temperature":0.7, })
-df = pd.read_csv('mall_customers_data.csv')
-agent = create_pandas_dataframe_agent(llm, df, verbose=True, max_iterations=6)
-# Setup streamlit app
-# Display the page title and the text box for the user to ask the question
-st.title('✨ Query your Data ')
-prompt = st.text_input("Enter your question to query your PDF documents")
-if prompt:
-    # Get the resonse from LLM
-    # We pass the model name (3.5) and the temperature (Closer to 1 means creative resonse)
-    # stuff chain type sends all the relevant text chunks from the document to LLM
-
-    response =  agent.run(prompt)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
-    # Write the results from the LLM to the UI
-    st.write("<b>" + prompt + "</b><br><i>" + response + "</i><hr>", unsafe_allow_html=True )
+def get_vectorstore(text_chunks):
+   # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-  
+
+def get_conversation_chain(vectorstore):
+    #llm = ChatOpenAI()
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm=HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+                   model_kwargs={"max_new_tokens":1200,
+                                 "top_k":40,
+                                 "temperature":0.7,
+                                 "top_p":0.5,
+                                 #"repetition_penalty":3,
+                                # "decoding":"top_p",
+                                 })
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
